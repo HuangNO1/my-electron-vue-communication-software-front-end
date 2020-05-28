511a54d08867bd2cc3b0d66f84b10aa6fd7e04bf
Huang Po-Hsun <fh831.cp9gw@gmail.com>
2020-05-28 17:12:23 +0800

    再次微調

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,15 +1,30 @@
 <template>
   <v-container fluid>
-    <div
-      style="width: 100%"
-    >
+    <div style="width: 100%">
       <div id="messageBox" class="white--text">
-        <div v-for="i in 100" :key="i">hhhhhh <br /></div>
-        <div>
-          endgggggggggggggggg
-        </div>
-        <div>
-          end
+        <div v-for="(item, i) in messages" :key="i">
+          <div v-if="item.fromId === '222'">
+            <v-avatar size="24" color="red">
+              <img
+                :src="item.fromAvatar"
+                alt="alt"
+              />
+            </v-avatar>
+            <v-chip class="ma-2" color="primary" dark label>
+              {{item.msg}}
+            </v-chip>
+          </div>
+          <div v-if="item.fromId === '333'">
+            <v-avatar size="24" color="pink">
+              <img
+                :src="item.fromAvatar"
+                alt="alt"
+              />
+            </v-avatar>
+            <v-chip class="ma-2" color="pink" dark label>
+              {{item.msg}}
+            </v-chip>
+          </div>
         </div>
       </div>
     </div>
@@ -19,10 +34,88 @@
 export default {
   name: "Chat",
   data() {
-      return {
-
-      };
-  }
+    return {
+      messages: [
+        {
+          userId: "",
+          fromId: "333",
+          toId: "222",
+          ip: "",
+          type: "",
+          msg: "Hello",
+          fromAvatar: "https://randomuser.me/api/portraits/men/78.jpg",
+          fromName: "333",
+          action: "send",
+        },
+        {
+          userId: "",
+          fromId: "333",
+          toId: "222",
+          ip: "",
+          type: "",
+          msg: "路遙X俊俊",
+          fromAvatar: "https://randomuser.me/api/portraits/men/78.jpg",
+          fromName: "333",
+          action: "send",
+        },
+        {
+          userId: "",
+          fromId: "222",
+          toId: "333",
+          ip: "",
+          type: "",
+          msg: "好爽喔",
+          fromAvatar: "https://avatars1.githubusercontent.com/u/48636976?s=460&u=6fc910ffe23ff8ff7ffc210d49ca81fdec486f9f&v=4",
+          fromName: "222",
+          action: "send",
+        },
+        {
+          userId: "",
+          fromId: "222",
+          toId: "333",
+          ip: "",
+          type: "",
+          msg: "多少錢",
+          fromAvatar: "https://avatars1.githubusercontent.com/u/48636976?s=460&u=6fc910ffe23ff8ff7ffc210d49ca81fdec486f9f&v=4",
+          fromName: "222",
+          action: "send",
+        },
+        {
+          userId: "",
+          fromId: "333",
+          toId: "222",
+          ip: "",
+          type: "",
+          msg: "是俊俊的話是免費的喔",
+          fromAvatar: "https://randomuser.me/api/portraits/men/78.jpg",
+          fromName: "333",
+          action: "send",
+        },
+        {
+          userId: "",
+          fromId: "222",
+          toId: "333",
+          ip: "",
+          type: "",
+          msg: "俊俊真幸福",
+          fromAvatar: "https://avatars1.githubusercontent.com/u/48636976?s=460&u=6fc910ffe23ff8ff7ffc210d49ca81fdec486f9f&v=4",
+          fromName: "222",
+          action: "send",
+        },
+        {
+          userId: "",
+          fromId: "333",
+          toId: "222",
+          ip: "",
+          type: "",
+          msg: "俊俊真幸福",
+          fromAvatar: "https://randomuser.me/api/portraits/men/78.jpg",
+          fromName: "333",
+          action: "send",
+        },
+      ],
+    };
+  },
 };
 </script>
 <style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,5 +1,6 @@
 <template>
   <div style="background-color: #212121; height: 100%; padding-bottom: 2rem;">
+    <!-- 最左側的欄位 -->
     <v-navigation-drawer color="grey darken-4" mini-variant permanent dark app>
       <v-list nav class="py-0">
         <v-list-item class="px-0">
@@ -15,7 +16,7 @@
           </v-list-item-content>
         </v-list-item>
 
-        <v-list-item v-for="(item, i) in items" :key="item.i" link>
+        <v-list-item v-for="(item, i) in chatItems" :key="i" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -37,6 +38,7 @@
         </v-list-item>
       </template>
     </v-navigation-drawer>
+    <!-- 隱藏的個人欄 -->
     <v-navigation-drawer
       v-model="drawer"
       color="#212121"
@@ -57,7 +59,7 @@
       <v-divider></v-divider>
 
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="item.i" link>
+        <v-list-item v-for="(item, i) in settingItems" :key="i" link>
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -86,6 +88,7 @@
               :active="['r']"
               style="overflow-y: auto; overflow-x: hidden;"
             >
+              <!-- 聊天欄 -->
               <v-list
                 class="resizable-content"
                 height="100%"
@@ -93,64 +96,49 @@
                 subheader
                 dark
               >
-                <v-subheader inset>Folders</v-subheader>
+                <v-subheader inset>Channels</v-subheader>
 
-                <v-list-item
-                  v-for="(item, i) in TESTitems"
-                  :key="item.i"
-                  @click=""
-                  dark
-                >
-                  <v-list-item-avatar>
-                    <v-icon
-                      :class="[item.iconClass]"
-                      v-text="item.icon"
-                    ></v-icon>
-                  </v-list-item-avatar>
-
-                  <v-list-item-content>
-                    <v-list-item-title v-text="item.title"></v-list-item-title>
-                    <v-list-item-subtitle
-                      v-text="item.subtitle"
-                    ></v-list-item-subtitle>
-                  </v-list-item-content>
-
-                  <v-list-item-action>
-                    <v-btn icon>
-                      <v-icon color="grey lighten-1">mdi-information</v-icon>
-                    </v-btn>
-                  </v-list-item-action>
+                <v-list-item>
+                  <v-text-field
+                    label="Solo"
+                    placeholder="Search Channel"
+                    solo
+                  ></v-text-field>
                 </v-list-item>
-
-                <v-divider inset></v-divider>
-
-                <v-subheader inset>Files</v-subheader>
-
-                <v-list-item
-                  v-for="(item, i) in TESTitems2"
-                  :key="item.i"
-                  @click=""
+                <v-list-item-group
+                  v-model="selectItem"
+                  mandatory
+                  color="primary"
                 >
-                  <v-list-item-avatar>
-                    <v-icon
-                      :class="[item.iconClass]"
-                      v-text="item.icon"
-                    ></v-icon>
-                  </v-list-item-avatar>
+                  <v-list-item
+                    v-for="(item, i) in messageItems"
+                    :key="i"
+                    @click=""
+                    dark
+                  >
+                    <v-list-item-avatar>
+                      <v-icon
+                        :class="[item.iconClass]"
+                        v-text="item.icon"
+                      ></v-icon>
+                    </v-list-item-avatar>
 
-                  <v-list-item-content>
-                    <v-list-item-title v-text="item.title"></v-list-item-title>
-                    <v-list-item-subtitle
-                      v-text="item.subtitle"
-                    ></v-list-item-subtitle>
-                  </v-list-item-content>
+                    <v-list-item-content>
+                      <v-list-item-title
+                        v-text="item.title"
+                      ></v-list-item-title>
+                      <v-list-item-subtitle
+                        v-text="item.subtitle"
+                      ></v-list-item-subtitle>
+                    </v-list-item-content>
 
-                  <v-list-item-action>
-                    <v-btn icon>
-                      <v-icon color="grey lighten-1">mdi-information</v-icon>
-                    </v-btn>
-                  </v-list-item-action>
-                </v-list-item>
+                    <v-list-item-action>
+                      <v-btn icon>
+                        <v-icon color="grey lighten-1">mdi-information</v-icon>
+                      </v-btn>
+                    </v-list-item-action>
+                  </v-list-item>
+                </v-list-item-group>
               </v-list>
             </vue-resizable>
           </div>
@@ -191,14 +179,19 @@ export default {
   },
   data() {
     return {
-      items: [
+      selectItem: 0,
+      chatItems: [
         { title: "Chat", icon: "mdi-forum" },
         { title: "Personal", icon: "mdi-account" },
         { title: "Group", icon: "mdi-account-group" },
       ],
       drawer: null,
-      links: ["Home", "Contacts", "Settings"],
-      TESTitems: [
+      settingItems: [
+        { title: "Chat", icon: "mdi-forum" },
+        { title: "New Group", icon: "mdi-account-multiple" },
+        { title: "Logout", icon: "mdi-logout-variant" },
+      ],
+      messageItems: [
         {
           icon: "folder",
           iconClass: "grey lighten-1 white--text",
@@ -260,20 +253,6 @@ export default {
           subtitle: "Jan 28, 2014",
         },
       ],
-      TESTitems2: [
-        {
-          icon: "assignment",
-          iconClass: "blue white--text",
-          title: "Vacation itinerary",
-          subtitle: "Jan 20, 2014",
-        },
-        {
-          icon: "call_to_action",
-          iconClass: "amber white--text",
-          title: "Kitchen remodel",
-          subtitle: "Jan 10, 2014",
-        },
-      ],
     };
   },
 };
@@ -302,6 +281,6 @@ export default {
 
 #footer {
   padding: 1rem 1rem 1rem 25rem;
-  background-color: rbga(0,0,0,0)
+  background-color: rbga(0, 0, 0, 0);
 }
 </style>
